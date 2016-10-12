f197e56ddc1add435b99d9625cbf39c05ffcd33a
Robert Davis <robert@quarx.co>
2016-10-11 22:37:44 -0400

    Update main.cpp

--- Fibonacci/main.cpp
+++ Fibonacci/main.cpp
@@ -1,15 +1,62 @@
-//
-//  main.cpp
-//  Fibonacci
-//
-//  Created by Robert Davis on 10/11/16.
-//  Copyright © 2016 Robert Davis. All rights reserved.
-//
-
 #include <iostream>
+#include <stdlib.h>
+#include <curl/curl.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int fibonacci(long int n){
+    
+    if (n == 0)
+        return 0;
+    
+    else if (n == 1)
+        return 1;
+    
+    else
+        return (fibonacci(n-1) + fibonacci(n-2));
+}
+
+
+
+
+int main(void){
+    
+    int num;
+    int i = 0;
+    int array[20];
+    
+    FILE *numbers;
+    FILE *result;
+    
+    result = fopen("/Users/robert/result.html","w");
+    
+    if ( result == NULL) {
+        printf ("File not created okay, errno = %d\n", errno);
+        return 1;
+    }
+
+    
+    numbers = fopen("/Users/robert/codingtest.txt","r");
+    
+    if ( numbers == NULL) {
+        printf ("File not created okay, errno = %d\n", errno);
+        return 1;
+    }
+    
+    while(fscanf(numbers, "%d,", &num) > 0) {
+        array[i] = num;
+        i++;
+    }
+    fclose(numbers);
+    
+    
+    for(int i = 0; i < 6; i++){
+    
+        fprintf(result,"%d, \n",fibonacci(array[i]));
+        
+    }
+    fclose(numbers);
+    fclose(result);
+    
     return 0;
 }
